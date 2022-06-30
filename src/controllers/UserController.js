b42bcf08510a0ea450ab0e2f14f5dcc8bc04149c
ceifeirocv <jair.almeida.oliveira@gmail.com>
2022-06-30 15:02:14 -0100

    chore: add email and username vverification

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,7 +1,23 @@
+const { Op } = require('sequelize');
 const { Users } = require('../models');
 
 module.exports = {
   async store(req, res) {
+    const userExists = await Users.findOne({
+      where: {
+        [Op.or]: [
+          { username: req.body.username },
+          { email: req.body.email },
+        ],
+      },
+    });
+    if (userExists.username === req.body.username) {
+      return res.status(404).json({ error: 'Username already taken.' });
+    }
+    if (userExists.email === req.body.email) {
+      return res.status(404).json({ error: 'User with provided email already exists' });
+    }
+
     const {
       id,
       username,
