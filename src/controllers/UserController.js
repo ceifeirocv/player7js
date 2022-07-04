7d3d48b1359cf09c8f8c95fc687f0e0c49b694ce
ceifeirocv <jair.almeida.oliveira@gmail.com>
2022-07-04 15:06:04 -0100

    chore: update Users

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -32,6 +32,7 @@ module.exports = {
       administrator,
     });
   },
+
   async getUsers(req, res) {
     const user = await Users.findAll({
       attributes: ['id', 'username', 'email'],
@@ -39,7 +40,40 @@ module.exports = {
 
     return res.json(user);
   },
+
   async update(req, res) {
-    return res.json({ id: req.userId, message: 'user updated' });
+    const {
+      email,
+      password: oldPassword,
+      newPassword: password,
+      newPasswordConfirm,
+    } = req.body;
+
+    const user = await Users.findByPk(req.userId);
+
+    if (!oldPassword || !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'password does not match' });
+    }
+
+    if (email) {
+      const userExists = await Users.findOne({ where: { email } });
+      if (userExists && userExists.email === email) {
+        return res.status(404).json({ error: 'User with provided email already exists' });
+      }
+    }
+    if (password !== newPasswordConfirm) return res.status(401).json({ error: 'passwords does not match' });
+
+    const {
+      id,
+      username,
+      administrator,
+    } = await user.update({ password, email });
+
+    return res.json({
+      id,
+      username,
+      email,
+      administrator,
+    });
   },
 };
