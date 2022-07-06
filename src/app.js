c072444c6f8d8f89264c5923d226a27faa217d7a
ceifeirocv <jair.almeida.oliveira@gmail.com>
2022-07-06 10:01:09 -0100

    refactor: refactoring app and server

---
+++ src/app.js
@@ -0,0 +1,32 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const { usersRoutes, sessionRoutes } = require('./routes');
+const jsonCheck = require('./middlewares/jsonCheck');
+const authMiddleware = require('./middlewares/auth');
+
+require('./models');
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(bodyParser.json());
+    this.server.use(jsonCheck);
+  }
+
+  routes() {
+    this.server.use('/users', authMiddleware, usersRoutes);
+    this.server.use('/sessions', sessionRoutes);
+    this.server.use((req, res) => {
+      res.status(400).send('404: Page not found');
+    });
+  }
+}
+
+module.exports = new App().server;
--- src/server.js
+++ src/server.js
@@ -1,24 +1,7 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const app = require('./app');
 
 require('dotenv').config();
-require('./models');
 
-const { usersRoutes, sessionRoutes } = require('./routes');
-const authMiddleware = require('./middlewares/auth');
-const jsonCheck = require('./middlewares/jsonCheck');
-
-const app = express();
 const PORT = process.env.PORT || 3333;
-
-app.use(bodyParser.json());
-app.use(jsonCheck);
-
-app.use('/users', authMiddleware, usersRoutes);
-app.use('/sessions', sessionRoutes);
-
-app.use((req, res) => {
-  res.status(400).send('404: Page not found');
-});
 
 app.listen(PORT, () => console.log(`Server Running on port: http://localhost:${PORT}`));
